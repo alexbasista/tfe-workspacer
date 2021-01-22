250f9d20016b311d3c31d350000eecc6ac8a33b3
alexbasista <alex.basista@gmail.com>
2021-01-22 10:53:55 -0500

    add test var to each ws

--- main.tf
+++ main.tf
@@ -11,17 +11,45 @@ provider "tfe" {
     hostname = "tfe.is.hashicorpdemo.com"
 }
 
+### --- ws --- ###
 resource "tfe_workspace" "appname-aws-webtier-1" {
   name         = "appname-aws-webtier-1"
   organization = "winterfell"
 }
 
+resource "tfe_variable" "test" {
+  key          = "my_key_name"
+  value        = "my_value_name"
+  category     = "terraform"
+  workspace_id = tfe_workspace.appname-aws-webtier-1.id
+  description  = "a useful description"
+}
+
+
+### --- ws --- ###
 resource "tfe_workspace" "appname-aws-middleware-1" {
   name         = "appname-aws-middleware-1"
   organization = "winterfell"
 }
 
+resource "tfe_variable" "test" {
+  key          = "my_key_name"
+  value        = "my_value_name"
+  category     = "terraform"
+  workspace_id = tfe_workspace.appname-aws-middleware-1.id
+  description  = "a useful description"
+}
+
+### --- ws --- ###
 resource "tfe_workspace" "appname-aws-database-1" {
   name         = "appname-aws-database-1"
   organization = "winterfell"
+}
+
+resource "tfe_variable" "test" {
+  key          = "my_key_name"
+  value        = "my_value_name"
+  category     = "terraform"
+  workspace_id = tfe_workspace.appname-aws-database-1.id
+  description  = "a useful description"
 }
